d01a4341cdac44d67c7478ea9e54908a6cda078b
aoissx <113966503+aoissx@users.noreply.github.com>
2023-10-20 09:53:53 +0900

    init

---
+++ src/main/java/net/isksss/mc/paperslot/Paper_slot.java
@@ -0,0 +1,17 @@
+package net.isksss.mc.paperslot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Paper_slot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
